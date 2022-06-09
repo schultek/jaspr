38e0f56132b7229e9358b2c69271cea687260933
Martin Jablečník <martin.jablecnik@email.cz>
2022-06-10 00:20:43 +0200

    Update jaspr_bootstrap.dart file

--- packages/jaspr_bootstrap/lib/jaspr_bootstrap.dart
+++ packages/jaspr_bootstrap/lib/jaspr_bootstrap.dart
@@ -2,8 +2,15 @@ library jaspr_bootstrap;
 
 export 'src/components/carousel.dart';
 export 'src/core/center.dart';
+export 'src/core/container.dart';
 export 'src/core/size.dart';
 export 'src/elements/button.dart';
 export 'src/elements/header.dart';
+export 'src/elements/horizontal_line.dart';
 export 'src/elements/image.dart';
 export 'src/elements/paragraph.dart';
+export 'src/edge_insets.dart';
+export 'src/enums.dart';
+export 'src/border.dart';
+
+
