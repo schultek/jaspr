7c54255722f5b262baf1568d405fabbea75fadeb
Martin Jablečník <martin.jablecnik@email.cz>
2022-06-10 00:21:32 +0200

    Add ContainerPage example

--- packages/jaspr_bootstrap/example/lib/app.dart
+++ packages/jaspr_bootstrap/example/lib/app.dart
@@ -1,9 +1,9 @@
-import 'package:example/pages/carousel_page.dart';
+import 'package:example/pages/container_page.dart';
 import 'package:jaspr/jaspr.dart';
 
 class App extends StatelessComponent {
   @override
   Iterable<Component> build(BuildContext context) sync* {
-    yield CarouselPage();
+    yield ContainerPage();
   }
 }
---
+++ packages/jaspr_bootstrap/example/lib/pages/container_page.dart
@@ -0,0 +1,88 @@
+import 'package:jaspr/jaspr.dart';
+import 'package:jaspr_bootstrap/jaspr_bootstrap.dart';
+
+class ContainerPage extends StatelessComponent {
+  @override
+  Iterable<Component> build(BuildContext context) sync* {
+    yield HorizontalLine();
+    yield Container(
+      children: [
+        Header("This is a heading", size: HeaderSize.h1),
+        Paragraph("This is a paragraph of text."),
+      ],
+    );
+    yield HorizontalLine();
+    yield Container(
+      breakpoint: Breakpoint.fluid,
+      children: [
+        Header("This is a heading", size: HeaderSize.h1),
+        Paragraph("This is a paragraph of text."),
+      ],
+    );
+    yield HorizontalLine();
+    yield Container(
+      breakpoint: Breakpoint.small,
+      child: Text("100% wide until screen size less than 576px"),
+    );
+    yield Container(
+      breakpoint: Breakpoint.medium,
+      child: Text("100% wide until screen size less than 768px"),
+    );
+    yield Container(
+      breakpoint: Breakpoint.large,
+      child: Text("100% wide until screen size less than 992px"),
+    );
+    yield Container(
+      breakpoint: Breakpoint.extraLarge,
+      child: Text("100% wide until screen size less than 1200px"),
+    );
+    yield HorizontalLine();
+    yield Container(
+      backgroundColor: BackgroundColor.dark,
+      textColor: TextColor.white,
+      border: Border(),
+      padding: EdgeInsets.symmetric(vertical: Space.s3),
+      margin: EdgeInsets.symmetric(vertical: Space.s3),
+      children: [
+        Header("This is a heading", size: HeaderSize.h1),
+        Paragraph("This is a paragraph of text."),
+      ],
+    );
+    yield Container(
+      backgroundColor: BackgroundColor.light,
+      padding: EdgeInsets.symmetric(vertical: Space.s3),
+      margin: EdgeInsets.symmetric(vertical: Space.s3),
+      children: [
+        Header("This is a heading", size: HeaderSize.h1),
+        Paragraph("This is a paragraph of text."),
+      ],
+    );
+    yield Container(
+      border: Border(edge: BorderEdge.all()),
+      padding: EdgeInsets.symmetric(vertical: Space.s3),
+      margin: EdgeInsets.symmetric(vertical: Space.s3),
+      children: [
+        Header("This is a heading", size: HeaderSize.h1),
+        Paragraph("This is a paragraph of text."),
+      ],
+    );
+    yield HorizontalLine();
+    yield Container(
+      border: Border(
+        edge: BorderEdge.all(),
+        color: BorderColor.primary,
+        round: BorderRound.all,
+        radius: BorderRadius.r5,
+        width: BorderWidth.w4,
+        opacity: BorderOpacity.o25,
+      ),
+      padding: EdgeInsets.symmetric(vertical: Space.s3),
+      margin: EdgeInsets.symmetric(vertical: Space.s3),
+      children: [
+        Header("This is a heading", size: HeaderSize.h1),
+        Paragraph("This is a paragraph of text."),
+      ],
+    );
+    yield HorizontalLine();
+  }
+}
