57e756c188d411489e644c803e38977cdb120880
Martin Jablečník <martin.jablecnik@email.cz>
2022-06-12 01:12:56 +0200

    Add pages library into examples

--- packages/jaspr_bootstrap/example/lib/app.dart
+++ packages/jaspr_bootstrap/example/lib/app.dart
@@ -1,14 +1,14 @@
-import 'package:example/pages/grid_system_page.dart';
-import 'package:example/pages/first_page.dart';
+import 'package:example/pages.dart';
 import 'package:jaspr/jaspr.dart';
 import 'package:jaspr_ui/core.dart';
+
 
 class App extends StatelessComponent {
   @override
   Iterable<Component> build(BuildContext context) sync* {
     yield PageStyle(
       style: GridSystemPage.style,
-      child: FirstPage(),
+      child: GridSystemPage(),
     );
   }
 }
