00f482f43046232c91c405970180d9ff5bbb292a
Martin Jablečník <martin.jablecnik@email.cz>
2022-07-02 10:08:48 +0200

    Add other units

--- packages/jaspr/lib/src/ui/styles/units.dart
+++ packages/jaspr/lib/src/ui/styles/units.dart
@@ -1,5 +1,5 @@
 class Unit {
-  final int value;
+  final double value;
   final String type;
 
   const Unit(this.value, {required this.type});
@@ -7,7 +7,7 @@ class Unit {
   @override
   String toString() => "$value$type";
 
-  factory Unit.custom(int value, String type) {
+  factory Unit.custom(double value, String type) {
     return Unit(value, type: type);
   }
 }
@@ -28,4 +28,22 @@ class Points extends Unit {
   const Points(super.value) : super(type: 'pt');
 
   static const Points zero = Points(0);
+}
+
+class Em extends Unit {
+  const Em(super.value) : super(type: 'em');
+
+  static const Em zero = Em(0);
+}
+
+class Rem extends Unit {
+  const Rem(super.value) : super(type: 'rem');
+
+  static const Rem zero = Rem(0);
+}
+
+class Number extends Unit {
+  const Number(super.value) : super(type: '');
+
+  static const Number zero = Number(0);
 }
