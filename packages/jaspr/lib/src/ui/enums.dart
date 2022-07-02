81f6731ac12bb1bd2a1143fef4898638a9183788
Martin Jablečník <martin.jablecnik@email.cz>
2022-07-02 10:09:54 +0200

    Add TextStyle

--- packages/jaspr/lib/src/ui/enums.dart
+++ packages/jaspr/lib/src/ui/enums.dart
@@ -35,3 +35,78 @@ enum Clip {
   final String value;
   const Clip(this.value);
 }
+
+enum FontFamily {
+  arial("Arial"),
+  helvetica("Helvetica"),
+  gillSans("'Gill Sans'"),
+  lucida("Lucida"),
+  helveticaNarrow("'Helvetica Narrow'"),
+  sansSerif("sans-serif"),
+  times("Times"),
+  timesNewRoman("'Times New Roman'"),
+  palatino("Palatino"),
+  bookman("Bookman"),
+  newCenturySchoolbook("'New Century Schoolbook'"),
+  serif("serif"),
+  andaleMono("'Andale Mono'"),
+  courierNew("'Courier New'"),
+  courier("Courier"),
+  lucidatypewriter("Lucidatypewriter"),
+  fixed("Fixed"),
+  monospace("monospace"),
+  comicSans("'Comic Sans'"),
+  zapfChancery("'Zapf Chancery'"),
+  coronetscript("Coronetscript"),
+  florence("Florence"),
+  parkavenue("Parkavenue"),
+  cursive("cursive"),
+  impact("Impact"),
+  arnoldboecklin("Arnoldboecklin"),
+  oldtown("Oldtown"),
+  blippo("Blippo"),
+  brushstroke("Brushstroke"),
+  fantasy("fantasy"),
+  ;
+
+  final String value;
+  const FontFamily(this.value);
+}
+
+enum Alignment { left, center, right, justify }
+
+enum FontStyle { normal, italic, oblique }
+
+enum TextTransformation { uppercase, capitalize, lowercase }
+
+enum FontWeight {
+  normal("normal"),
+  bold("bold"),
+  bolder("bolder"),
+  lighter("lighter"),
+  w100("100"),
+  w200("200"),
+  w300("300"),
+  w400("400"),
+  w500("500"),
+  w600("600"),
+  w700("700"),
+  w800("800"),
+  w900("900"),
+  inherit("inherit"),
+  ;
+
+  final String value;
+  const FontWeight(this.value);
+}
+
+enum TextDecoration {
+  none("none"),
+  underline("underline"),
+  overline("overline"),
+  lineThrough("line-through"),
+  ;
+
+  final String value;
+  const TextDecoration(this.value);
+}
--- packages/jaspr/lib/src/ui/styles.dart
+++ packages/jaspr/lib/src/ui/styles.dart
@@ -3,4 +3,5 @@ library styles;
 export 'styles/background.dart';
 export 'styles/base.dart';
 export 'styles/color.dart';
+export 'styles/text.dart';
 export 'styles/units.dart';
