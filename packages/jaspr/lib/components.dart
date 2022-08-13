c084a91e80a17e2ef93c9c7c23a1422f0c7f3804
Martin Jablečník <martin.jablecnik@email.cz>
2022-08-13 23:30:02 +0200

    Move Link component into other file

--- packages/jaspr/lib/components.dart
+++ packages/jaspr/lib/components.dart
@@ -1,6 +1,6 @@
 library components;
 
 export 'src/ui/components/base.dart';
-export 'src/ui/components/link.dart';
 export 'src/ui/components/media.dart';
+export 'src/ui/components/other.dart';
 export 'src/ui/components/text.dart';
