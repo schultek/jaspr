1dce29671170360b73ab3be0af634005519632a0
Martin Jablečník <martin.jablecnik@email.cz>
2022-08-13 21:41:11 +0200

    Add Link component

--- packages/jaspr/lib/components.dart
+++ packages/jaspr/lib/components.dart
@@ -2,4 +2,5 @@ library components;
 
 export 'src/ui/components/base.dart';
 export 'src/ui/components/header.dart';
+export 'src/ui/components/link.dart';
 export 'src/ui/components/texts.dart';
---
+++ packages/jaspr/lib/src/ui/components/link.dart
@@ -0,0 +1,26 @@
+import 'package:jaspr/src/ui/components/base.dart';
+
+class Link extends BaseElement {
+  final String href;
+  final bool openInNewTab;
+
+  const Link({
+    required this.href,
+    this.openInNewTab = false,
+    super.key,
+    super.id,
+    super.styles,
+    super.classes,
+    super.attributes,
+    super.events,
+    super.child,
+    super.children,
+  }) : super(tag: 'a');
+
+  @override
+  Map<String, String> getAttributes() => {
+        "href": href,
+        if (openInNewTab) "target": "_blank",
+        ...super.attributes ?? {},
+      };
+}
