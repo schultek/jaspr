c424b5a6733996185f626feef27e5412d46944b5
Martin Jablečník <martin.jablecnik@email.cz>
2022-08-30 20:30:57 +0200

    Export flexbox styles

--- packages/jaspr/lib/styles.dart
+++ packages/jaspr/lib/styles.dart
@@ -4,6 +4,7 @@ export 'src/ui/styles/properties/background.dart';
 export 'src/ui/styles/properties/box.dart';
 export 'src/ui/styles/properties/color.dart';
 export 'src/ui/styles/properties/edge_insets.dart';
+export 'src/ui/styles/properties/flexbox.dart';
 export 'src/ui/styles/properties/text.dart';
 export 'src/ui/styles/properties/unit.dart';
 export 'src/ui/styles/style.dart';
