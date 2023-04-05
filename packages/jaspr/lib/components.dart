4924836ded134404da1bf37f41705546f6910f76
Martin Jablečník <martin.jablecnik@email.cz>
2023-04-05 11:16:16 +0200

    Refactor List component

--- packages/jaspr/lib/components.dart
+++ packages/jaspr/lib/components.dart
@@ -4,7 +4,6 @@ export 'jaspr.dart' hide runApp;
 
 export 'src/ui/components/base.dart';
 export 'src/ui/components/layout.dart';
-export 'src/ui/components/list.dart';
 export 'src/ui/components/media.dart';
 export 'src/ui/components/other.dart';
 export 'src/ui/components/text.dart';
