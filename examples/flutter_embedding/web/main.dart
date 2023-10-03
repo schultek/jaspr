d60e1d8dbb02112da7ada04ae6c034b4ee1b6bdd
Kilian Schulte <schultek@in.tum.de>
2023-10-03 16:24:13 +0200

    fix flutter example

--- examples/flutter_embedding/web/main.dart
+++ examples/flutter_embedding/web/main.dart
@@ -1,4 +1,4 @@
-import 'package:element_embedding_demo/components/app.dart';
+import 'package:flutter_embedding_demo/components/app.dart';
 import 'package:jaspr/browser.dart';
 
 /// The web entrypoint for the jaspr app.
