1dbcbc47da42ad9772d9dfb7f8506a52526e4236
Kilian Schulte <schultek@in.tum.de>
2023-10-04 14:06:00 +0200

    change analytics implementation

--- packages/jaspr_cli/lib/src/analytics.dart
+++ packages/jaspr_cli/lib/src/analytics.dart
@@ -1,3 +1,4 @@
+import 'dart:convert';
 import 'dart:io';
 
 import 'package:http/http.dart';
@@ -9,17 +10,25 @@ Future<void> trackEvent(
   String command, {
   String? projectName,
 }) async {
-  var projectId = projectName != null ? hash(projectName) : null;
+  try {
+    var systemId = hash(join(Platform.localHostname, Platform.operatingSystem, Platform.operatingSystemVersion));
 
-  var userId = hash(join(Platform.localHostname, Platform.operatingSystem, Platform.operatingSystemVersion));
-
-  var cliVersion = jasprCliVersion;
-
-  await get(Uri.parse('https://www.woopra.com/track/ce?'
-      'project=jasprpad.schultek.de'
-      '&event=cli_usage'
-      '&ce_command=$command'
-      '&ce_cli_version=$cliVersion'
-      '${projectId != null ? '&ce_project_id=$projectId' : ''}'
-      '&cv_id=$userId'));
+    await post(
+      Uri.parse('https://api-eu.mixpanel.com/track'),
+      headers: {HttpHeaders.contentTypeHeader: 'application/json'},
+      body: jsonEncode([
+        {
+          'event': 'cli_usage',
+          'properties': {
+            'token': '4de26246ad502c15ea6b0e5b9137a95c',
+            'distinct_id': '$systemId',
+            'command': command,
+            'cli_version': jasprCliVersion,
+            if (projectName != null) 'project_id': '${hash(projectName)}',
+            'os': Platform.operatingSystem,
+          }
+        }
+      ]),
+    );
+  } catch (_) {}
 }
