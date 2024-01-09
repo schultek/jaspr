9473521bde4a309e26a9acbf2541dcc6023cfee8
Kilian Schulte <schultek@in.tum.de>
2024-01-10 00:18:07 +0100

    update hidden and splitter components

--- apps/jaspr_pad/lib/components/elements/hidden.dart
+++ apps/jaspr_pad/lib/components/elements/hidden.dart
@@ -1,7 +1,12 @@
 import 'package:jaspr/jaspr.dart';
 
 class Hidden extends StatelessComponent {
-  const Hidden({required this.hidden, required this.child, this.visibilityMode = false, Key? key}) : super(key: key);
+  const Hidden({
+    required this.hidden,
+    required this.child,
+    this.visibilityMode = false,
+    Key? key,
+  }) : super(key: key);
 
   final bool hidden;
   final Component child;
@@ -9,28 +14,12 @@ class Hidden extends StatelessComponent {
 
   @override
   Iterable<Component> build(BuildContext context) sync* {
-    yield RenderScope(
-      delegate: HiddenRenderDelegate(hidden, visibilityMode),
+    yield DomComponent.wrap(
+      styles: Styles.box(
+        visibility: hidden && visibilityMode ? Visibility.hidden : null,
+      ),
+      attributes: {if (hidden && !visibilityMode) 'hidden': ''},
       child: child,
     );
-  }
-}
-
-class HiddenRenderDelegate extends RenderDelegate {
-  HiddenRenderDelegate(this.hidden, this.visibilityMode);
-
-  final bool hidden;
-  final bool visibilityMode;
-
-  @override
-  void renderNode(RenderElement node, String tag, String? id, List<String>? classes, Map<String, String>? styles,
-      Map<String, String>? attributes, Map<String, EventCallback>? events) {
-    super.renderNode(node, tag, id, classes, {...?styles, if (hidden && visibilityMode) 'visibility': 'hidden'},
-        {...?attributes, if (hidden && !visibilityMode) 'hidden': ''}, events);
-  }
-
-  @override
-  bool updateShouldNotify(covariant HiddenRenderDelegate oldDelegate) {
-    return hidden != oldDelegate.hidden || visibilityMode != oldDelegate.visibilityMode;
   }
 }
