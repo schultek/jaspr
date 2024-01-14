dbd87f7fb28a5dc70afb2f30f1d6d7c2ad0f4b1c
Rishabh Singh <rishabhsingh@berkeley.edu>
2024-01-13 20:55:28 -0500

    added color transparent

--- packages/jaspr/lib/src/ui/styles/properties/color.dart
+++ packages/jaspr/lib/src/ui/styles/properties/color.dart
@@ -12,13 +12,15 @@ abstract class Color {
   const factory Color.rgb(int red, int green, int blue) = _RGBColor;
 
   /// Constructs a [Color] from red, green, blue and alpha values
-  const factory Color.rgba(int red, int green, int blue, double alpha) = _RGBAColor;
+  const factory Color.rgba(int red, int green, int blue, double alpha) =
+      _RGBAColor;
 
   /// Constructs a [Color] from hue, saturation and lightness values
   const factory Color.hsl(int hue, int saturation, int lightness) = _HSLColor;
 
   /// Constructs a [Color] from hue, saturation, lightness and alpha values
-  const factory Color.hsla(int hue, int saturation, int lightness, double alpha) = _HSLAColor;
+  const factory Color.hsla(
+      int hue, int saturation, int lightness, double alpha) = _HSLAColor;
 
   static const Color inherit = Color.named('inherit');
   static const Color initial = Color.named('initial');
@@ -236,4 +238,5 @@ class Colors {
   static const Color whiteSmoke = Color.named('whiteSmoke');
   static const Color yellow = Color.named('yellow');
   static const Color yellowGreen = Color.named('yellowGreen');
+  static const Color transparent = Color.named('transparent');
 }
