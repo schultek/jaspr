8cfee904acbe314502c8362f2ed4db79ceeb0e6d
Kilian Schulte <schultek@in.tum.de>
2024-01-19 12:40:58 +0100

    fix example

--- examples/package_riverpod/lib/main.dart
+++ examples/package_riverpod/lib/main.dart
@@ -1,12 +1,13 @@
 import 'package:jaspr/server.dart';
 
 import 'components/app.dart';
+import 'jaspr_options.dart';
 
 /// Entrypoint for the server
 void main() async {
-  // Jaspr.initializeApp(
-  //   options: defaultJasprOptions,
-  // );
+  Jaspr.initializeApp(
+    options: defaultJasprOptions,
+  );
 
   runApp(
     Document(body: App()),
