4f72314f02d05cb2658db21c7f2cbf8bebe1b43f
Kilian Schulte <schultek@in.tum.de>
2024-03-14 11:13:22 +0100

    add proper support for raw html

--- packages/jaspr/lib/server.dart
+++ packages/jaspr/lib/server.dart
@@ -4,6 +4,7 @@ export 'package:shelf/shelf.dart';
 
 export 'jaspr.dart' hide runApp, AppBinding;
 export 'src/server/document/document.dart';
+export 'src/server/markup_render_object.dart';
 export 'src/server/run_app.dart';
 export 'src/server/server_app.dart';
 export 'src/server/server_binding.dart';
--- packages/jaspr/lib/src/components/html/html.dart
+++ packages/jaspr/lib/src/components/html/html.dart
@@ -2,6 +2,7 @@ import '../../foundation/basic_types.dart';
 import '../../foundation/events/events.dart';
 import '../../foundation/styles/styles.dart';
 import '../../framework/framework.dart';
+import '../raw_text/raw_text.dart';
 
 part 'content.dart';
 part 'forms.dart';
@@ -13,6 +14,10 @@ part 'text.dart';
 final _events = events;
 
 /// Utility method to create a text component when using jaspr html methods
-Component text(String text, {bool rawHtml = false}) {
-  return Text(text, rawHtml: rawHtml);
+Component text(String text) {
+  return Text(text);
+}
+
+Component raw(String text) {
+  return RawText(text);
 }
