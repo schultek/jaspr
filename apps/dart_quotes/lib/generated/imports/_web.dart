d5fc67575685160f73f714fe6ff8ae20972d03dd
Kilian Schulte <schultek@in.tum.de>
2024-05-24 13:03:52 +0200

    cleanup dart_quotes

--- apps/dart_quotes/lib/generated/imports/_web.dart
+++ apps/dart_quotes/lib/generated/imports/_web.dart
@@ -3,17 +3,18 @@
 
 // ignore_for_file: directives_ordering
 
+import '../../../interop/confetti.dart' show JSConfetti;
 import 'package:firebase_core/firebase_core.dart' show Firebase, FirebaseApp;
 import 'package:cloud_firestore/cloud_firestore.dart' show FirebaseFirestore, FieldValue, DocumentSnapshot;
 import 'package:firebase_auth/firebase_auth.dart' show FirebaseAuth;
 import '../../../firebase_options.dart' show DefaultFirebaseOptions;
-import '../../../interop/confetti.dart' show JSConfetti;
+export '../../../interop/confetti.dart' show JSConfetti;
 export 'package:firebase_core/firebase_core.dart' show Firebase, FirebaseApp;
 export 'package:cloud_firestore/cloud_firestore.dart' show FirebaseFirestore, FieldValue, DocumentSnapshot;
 export 'package:firebase_auth/firebase_auth.dart' show FirebaseAuth;
 export '../../../firebase_options.dart' show DefaultFirebaseOptions;
-export '../../../interop/confetti.dart' show JSConfetti;
 
+typedef JSConfettiOrStubbed = JSConfetti;
 typedef FirebaseOrStubbed = Firebase;
 typedef FirebaseAppOrStubbed = FirebaseApp;
 typedef FirebaseFirestoreOrStubbed = FirebaseFirestore;
@@ -21,4 +22,3 @@ typedef FieldValueOrStubbed = FieldValue;
 typedef DocumentSnapshotOrStubbed = DocumentSnapshot;
 typedef FirebaseAuthOrStubbed = FirebaseAuth;
 typedef DefaultFirebaseOptionsOrStubbed = DefaultFirebaseOptions;
-typedef JSConfettiOrStubbed = JSConfetti;
