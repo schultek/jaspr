ed772a3d5fbb5e83e3a70b73055c530c87f1076e
Kilian Schulte <schultek@in.tum.de>
2024-08-08 14:27:55 +0200

    update riverpod example

--- examples/package_riverpod/lib/components/app.dart
+++ examples/package_riverpod/lib/components/app.dart
@@ -2,7 +2,6 @@ import 'package:jaspr/jaspr.dart';
 import 'package:jaspr_riverpod/jaspr_riverpod.dart';
 
 import 'counter.dart';
-import 'counter2.dart';
 
 /// App component that displays a simple counter.
 ///
@@ -13,9 +12,6 @@ import 'counter2.dart';
 class App extends StatelessComponent {
   @override
   Iterable<Component> build(BuildContext context) sync* {
-    yield Counter2();
-    yield hr();
-    yield Counter2();
     yield ProviderScope(child: Counter());
   }
 }
--- examples/package_riverpod/lib/models/color.dart
+++
@@ -1,9 +0,0 @@
-import 'package:jaspr/jaspr.dart';
-
-extension type ColorCodec(Color value) implements Color {
-  @decoder
-  ColorCodec.decode(String c) : value = Color.hex(c);
-
-  @encoder
-  String encode() => value.value;
-}
