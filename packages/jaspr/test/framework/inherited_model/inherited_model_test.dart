e1c98934894ea849e218b271777547893601fae3
ahmednfwela <afwela@inpro.sa>
2024-09-15 10:58:32 +0300

    tests: simplify tests

--- packages/jaspr/test/framework/inherited_model/inherited_model_test.dart
+++ packages/jaspr/test/framework/inherited_model/inherited_model_test.dart
@@ -6,88 +6,71 @@ import 'package:jaspr_test/jaspr_test.dart';
 import '../../utils/test_component.dart';
 import 'inherited_model_app.dart';
 
+void testAgainstDto(MyDto src, {required Set<Key> expectedRebuilds}) {
+  expect(find.text('A: ${src.a}'), findsOneComponent);
+  expect(find.text('B: ${src.b}'), findsOneComponent);
+
+  // build should be called initially.
+  expect(buildCalledFor, equals(expectedRebuilds));
+
+  // reset tracking of build
+  buildCalledFor.clear();
+}
+
 void main() {
   group('inherited model test', () {
-    testComponents('should inherit model', (tester) async {
+    testComponents('should update dependencies only', (tester) async {
       var controller = await tester.pumpTestComponent(App());
+      Future<void> rebuildThenTestAgainstDto(
+        MyDto src, {
+        required Set<Key> expectedRebuilds,
+      }) async {
+        await controller.rebuildWith(src);
+        testAgainstDto(src, expectedRebuilds: expectedRebuilds);
+      }
 
       // phase 1: inherited component should be mounted
-      expect(find.text('A: 0'), findsOneComponent);
-      expect(find.text('B: 0.0'), findsOneComponent);
-
-      // build should be called initially.
-      expect(buildCalledFor, <Key>{
-        App.componentKey,
-        ExampleComponent.componentKey,
-        AComponent.componentKey,
-        BComponent.componentKey,
-      });
-
-      // reset tracking of build
-      buildCalledFor.clear();
+      testAgainstDto(
+        MyDto(a: 0, b: 0),
+        expectedRebuilds: {
+          App.componentKey,
+          ExampleComponent.componentKey,
+          AComponent.componentKey,
+          BComponent.componentKey,
+        },
+      );
 
       // phase 2: component should be update when inherited value changes
-      await controller.rebuildWith(MyDto(a: 1, b: 2.5));
-      expect(find.text('A: 1'), findsOneComponent);
-      expect(find.text('B: 2.5'), findsOneComponent);
-
-      // since both A and B have changed, both components should be rebuilt.
-      expect(buildCalledFor, <Key>{
+      await rebuildThenTestAgainstDto(MyDto(a: 1, b: 2.5), expectedRebuilds: {
         App.componentKey,
         // ExampleComponent should NOT be rebuilt
         AComponent.componentKey,
         BComponent.componentKey,
       });
-
-      // reset tracking of build
-      buildCalledFor.clear();
 
       // phase 3: only b has changed, a should not rebuild
-      await controller.rebuildWith(MyDto(a: 1, b: 3.5));
-      expect(find.text('A: 1'), findsOneComponent);
-      expect(find.text('B: 3.5'), findsOneComponent);
-
-      // since only B has changed, only BComponent should be rebuilt.
-      expect(buildCalledFor, <Key>{
+      await rebuildThenTestAgainstDto(MyDto(a: 1, b: 3.5), expectedRebuilds: {
         App.componentKey,
         // ExampleComponent should NOT be rebuilt
         // AComponent should NOT be rebuilt
         BComponent.componentKey,
       });
 
-      // reset tracking of build
-      buildCalledFor.clear();
-
       // phase 4: only a has changed, b should not rebuild
-      await controller.rebuildWith(MyDto(a: 2, b: 3.5));
-      expect(find.text('A: 2'), findsOneComponent);
-      expect(find.text('B: 3.5'), findsOneComponent);
-
-      // since only A has changed, only AComponent should be rebuilt.
-      expect(buildCalledFor, <Key>{
+      await rebuildThenTestAgainstDto(MyDto(a: 2, b: 3.5), expectedRebuilds: {
         App.componentKey,
         // ExampleComponent should NOT be rebuilt
-        // BComponent should NOT be rebuilt
         AComponent.componentKey,
+        // BComponent should NOT be rebuilt
       });
 
-      // reset tracking of build
-      buildCalledFor.clear();
-
       // phase 5: nothing changed, nothing should be rebuilt.
-      await controller.rebuildWith(MyDto(a: 2, b: 3.5));
-      expect(find.text('A: 2'), findsOneComponent);
-      expect(find.text('B: 3.5'), findsOneComponent);
-      
-      expect(buildCalledFor, <Key>{
+      await rebuildThenTestAgainstDto(MyDto(a: 2, b: 3.5), expectedRebuilds: {
         App.componentKey,
         // ExampleComponent should NOT be rebuilt
         // BComponent should NOT be rebuilt
         // AComponent should NOT be rebuilt
       });
-
-      // reset tracking of build
-      buildCalledFor.clear();
     });
   });
 }
