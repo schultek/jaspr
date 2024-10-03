76ae2d3475c6c623229f2e577a691400aa6dbedc
Kilian Schulte <schultek@in.tum.de>
2024-10-03 15:37:30 +0200

    add hydration tests

--- packages/jaspr/lib/src/browser/clients.dart
+++ packages/jaspr/lib/src/browser/clients.dart
@@ -17,7 +17,7 @@ void registerClientsSync(Map<String, ClientBuilder> clients) {
 void registerClients(Map<String, ClientLoader> clients) {
   final Map<String, ClientBuilder> builders = {};
   _applyClients((name) {
-    var client = (builders[name] ?? clients[name]!()) as FutureOr<ClientBuilder>;
+    final client = (builders[name] ?? clients[name]!()) as FutureOr<ClientBuilder>;
     if (client is ClientBuilder) {
       return builders[name] = client;
     } else {
@@ -44,30 +44,32 @@ void _applyClients(FutureOr<ClientBuilder> Function(String) fn) {
 
   web.Comment? currNode;
   while ((currNode = iterator.nextNode() as web.Comment?) != null) {
-    var value = currNode!.nodeValue ?? '';
+    final value = currNode!.nodeValue ?? '';
     var match = _compStartRegex.firstMatch(value);
     if (match != null) {
-      var name = match.group(1)!;
-      var data = match.group(2);
+      final name = match.group(1)!;
+      final data = match.group(2);
 
       nodes.add((name, data, currNode));
     }
 
     match = _compEndRegex.firstMatch(value);
     if (match != null) {
-      var name = match.group(1)!;
+      final name = match.group(1)!;
 
       if (nodes.last.$1 == name) {
-        var comp = nodes.removeLast();
-        var start = comp.$3;
+        final comp = nodes.removeLast();
+        final start = comp.$3;
         assert(start.parentNode == currNode.parentNode);
 
-        var between = (start, currNode);
+        final between = (start, currNode);
 
         // Remove the data string.
         start.text = '\$${comp.$1}';
 
-        var params = comp.$2 != null ? jsonDecode(unescapeMarkerText(comp.$2!)) : {};
+        final params = comp.$2 != null //
+            ? jsonDecode(unescapeMarkerText(comp.$2!)) as Map<String, dynamic>
+            : <String, dynamic>{};
         unawaited(_runBuilder(name, fn(name), params, between));
       }
     }
