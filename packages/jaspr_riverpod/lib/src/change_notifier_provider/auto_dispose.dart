3c949d0bccecff9270ac59db27c1d13ef229d859
Kilian Schulte <schultek@in.tum.de>
2024-11-23 15:50:09 +0100

    fix formatting

--- packages/jaspr_riverpod/lib/src/change_notifier_provider/auto_dispose.dart
+++ packages/jaspr_riverpod/lib/src/change_notifier_provider/auto_dispose.dart
@@ -4,10 +4,8 @@ part of '../change_notifier_provider.dart';
 
 /// {@macro riverpod.provider_ref_base}
 @Deprecated('will be removed in 3.0.0, use Ref instead')
-abstract class AutoDisposeChangeNotifierProviderRef<
-        NotifierT extends ChangeNotifier?>
-    extends ChangeNotifierProviderRef<NotifierT>
-    implements AutoDisposeRef<NotifierT> {}
+abstract class AutoDisposeChangeNotifierProviderRef<NotifierT extends ChangeNotifier?>
+    extends ChangeNotifierProviderRef<NotifierT> implements AutoDisposeRef<NotifierT> {}
 
 // ignore: subtype_of_sealed_class
 /// {@macro riverpod.change_notifier_provider}
@@ -22,8 +20,7 @@ class AutoDisposeChangeNotifierProvider<NotifierT extends ChangeNotifier?>
     @Deprecated('Will be removed in 3.0.0') super.argument,
     @Deprecated('Will be removed in 3.0.0') super.debugGetCreateSourceHash,
   }) : super(
-          allTransitiveDependencies:
-              computeAllTransitiveDependencies(dependencies),
+          allTransitiveDependencies: computeAllTransitiveDependencies(dependencies),
         );
 
   /// An implementation detail of Riverpod
@@ -86,8 +83,7 @@ class AutoDisposeChangeNotifierProvider<NotifierT extends ChangeNotifier?>
 
 /// The element of [AutoDisposeChangeNotifierProvider].
 @Deprecated('will be removed in 3.0.0, use Ref instead')
-class AutoDisposeChangeNotifierProviderElement<
-        NotifierT extends ChangeNotifier?>
+class AutoDisposeChangeNotifierProviderElement<NotifierT extends ChangeNotifier?>
     extends ChangeNotifierProviderElement<NotifierT>
     with AutoDisposeProviderElementMixin<NotifierT>
     implements AutoDisposeChangeNotifierProviderRef<NotifierT> {
@@ -100,14 +96,13 @@ class AutoDisposeChangeNotifierProviderElement<
 
 // ignore: subtype_of_sealed_class
 /// The [Family] of [AutoDisposeChangeNotifierProvider].
-class AutoDisposeChangeNotifierProviderFamily<NotifierT extends ChangeNotifier?, Arg>
-    extends AutoDisposeFamilyBase<
-        // ignore: deprecated_member_use_from_same_package
-        AutoDisposeChangeNotifierProviderRef<NotifierT>,
-        NotifierT,
-        Arg,
-        NotifierT,
-        AutoDisposeChangeNotifierProvider<NotifierT>> {
+class AutoDisposeChangeNotifierProviderFamily<NotifierT extends ChangeNotifier?, Arg> extends AutoDisposeFamilyBase<
+    // ignore: deprecated_member_use_from_same_package
+    AutoDisposeChangeNotifierProviderRef<NotifierT>,
+    NotifierT,
+    Arg,
+    NotifierT,
+    AutoDisposeChangeNotifierProvider<NotifierT>> {
   /// The [Family] of [AutoDisposeChangeNotifierProvider].
   AutoDisposeChangeNotifierProviderFamily(
     super._createFn, {
@@ -116,8 +111,7 @@ class AutoDisposeChangeNotifierProviderFamily<NotifierT extends ChangeNotifier?,
   }) : super(
           providerFactory: AutoDisposeChangeNotifierProvider.internal,
           debugGetCreateSourceHash: null,
-          allTransitiveDependencies:
-              computeAllTransitiveDependencies(dependencies),
+          allTransitiveDependencies: computeAllTransitiveDependencies(dependencies),
         );
 
   /// {@macro riverpod.override_with}
@@ -128,8 +122,7 @@ class AutoDisposeChangeNotifierProviderFamily<NotifierT extends ChangeNotifier?,
       Arg arg,
     ) create,
   ) {
-    return FamilyOverrideImpl<NotifierT, Arg,
-        AutoDisposeChangeNotifierProvider<NotifierT>>(
+    return FamilyOverrideImpl<NotifierT, Arg, AutoDisposeChangeNotifierProvider<NotifierT>>(
       this,
       (arg) => AutoDisposeChangeNotifierProvider<NotifierT>.internal(
         (ref) => create(ref, arg),
