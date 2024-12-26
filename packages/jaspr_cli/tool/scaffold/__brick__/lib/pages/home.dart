464dd72523f565b696ea0139958077bc36e35cc3
Kilian Schulte <schultek@in.tum.de>
2024-12-26 14:43:10 +0100

    feat: add new logo

--- packages/jaspr_cli/tool/scaffold/__brick__/lib/pages/home.dart
+++ packages/jaspr_cli/tool/scaffold/__brick__/lib/pages/home.dart
@@ -36,7 +36,7 @@ class Home extends StatelessComponent {
   @override
   Iterable<Component> build(BuildContext context) sync* {
     yield section([
-      img(src: 'images/logo.png', width: 80),
+      img(src: 'images/logo.svg', width: 80),
       h1([text('Welcome')]),
       p([text('You successfully create a new Jaspr site.')]),
       div(styles: Styles.box(height: 100.px), []),
