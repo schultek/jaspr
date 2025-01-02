1052f373a50302852d1a255c3957ae9c077290af
gonft <gonft.paul@gmail.com>
2025-01-02 22:41:56 +0900

    refactor: update generated files via serverpod generate
    - Update generated files to reflect the renaming of _Modules to Modules and improved class structure using 'serverpod generate' command.

--- apps/dart_quotes_server/client/lib/src/protocol/client.dart
+++ apps/dart_quotes_server/client/lib/src/protocol/client.dart
@@ -44,8 +44,8 @@ class EndpointQuotes extends _i1.EndpointRef {
       );
 }
 
-class _Modules {
-  _Modules(Client client) {
+class Modules {
+  Modules(Client client) {
     auth = _i4.Caller(client);
   }
 
@@ -75,19 +75,21 @@ class Client extends _i1.ServerpodClientShared {
           connectionTimeout: connectionTimeout,
           onFailedCall: onFailedCall,
           onSucceededCall: onSucceededCall,
-          disconnectStreamsOnLostInternetConnection: disconnectStreamsOnLostInternetConnection,
+          disconnectStreamsOnLostInternetConnection:
+              disconnectStreamsOnLostInternetConnection,
         ) {
     quotes = EndpointQuotes(this);
-    modules = _Modules(this);
+    modules = Modules(this);
   }
 
   late final EndpointQuotes quotes;
 
-  late final _Modules modules;
+  late final Modules modules;
 
   @override
   Map<String, _i1.EndpointRef> get endpointRefLookup => {'quotes': quotes};
 
   @override
-  Map<String, _i1.ModuleEndpointCaller> get moduleLookup => {'auth': modules.auth};
+  Map<String, _i1.ModuleEndpointCaller> get moduleLookup =>
+      {'auth': modules.auth};
 }
