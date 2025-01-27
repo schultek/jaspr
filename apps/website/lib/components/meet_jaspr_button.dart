96407bbd6533d3b608803a500f78398108833b40
Kilian Schulte <git@schultek.dev>
2025-01-27 22:40:14 +0100

    refactor: improve website

--- apps/website/lib/components/meet_jaspr_button.dart
+++ apps/website/lib/components/meet_jaspr_button.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 import 'dart:math';
 
 import 'package:jaspr/jaspr.dart';
-import 'package:universal_web/web.dart';
+import 'package:universal_web/web.dart' as web;
 import 'package:website/components/link_button.dart';
 import 'package:website/constants/theme.dart';
 
@@ -15,99 +15,81 @@ class MeetJasprButton extends StatefulComponent {
 }
 
 class MeetJasprButtonState extends State<MeetJasprButton> {
-  double activation = 0;
+  final notifier = ProgressNotifier();
 
-  bool get activated => activation >= 100;
-  int get progress => activated ? 100 : max((activation - 10) / 0.9, 0).round();
+  var imageIndex = 0;
 
-  Timer? timer;
-
-  var particleCooldown = 0;
-
-  final List<({String id, int dx, int dy, int offset, int size})> particles = [];
-
-  final random = Random();
-
-  String randomId() {
-    return random.nextInt(0xFFFF).toRadixString(16);
+  @override
+  void initState() {
+    super.initState();
+    notifier.addListener(() {
+      setState(() {});
+      if (notifier.done) {
+        changeJasprText();
+      }
+    });
   }
 
-  void onActivate(double movement) {
-    if (activated) return;
-    if (activation >= 100) {
-      setState(() {
-        activation = 100;
-      });
-      timer?.cancel();
-      timer = null;
-      return;
+  void showRandomImage() {
+    if (imageIndex == 0) {
+      setupKeyListener();
     }
-
+    var current = imageIndex;
     setState(() {
-      activation += movement;
-    });
-
-    if (progress > 0) {
-      if (particleCooldown > 5) {
-        particleCooldown = 0;
+      while (imageIndex == current) {
+        imageIndex = Random().nextInt(18) + 1;
       }
-      if (particleCooldown == 0) {
-        var particle = (
-          id: randomId(),
-          dx: progress,
-          dy: random.nextInt(100),
-          offset: 10 + random.nextInt(40),
-          size: 1 + random.nextInt(2),
-        );
-        particles.add(particle);
+    });
+  }
 
-        Future.delayed(Duration(seconds: 1), () {
-          setState(() {
-            particles.remove(particle);
-          });
+  void setupKeyListener() {
+    late final StreamSubscription sub;
+    sub = web.window.onKeyDown.listen((event) {
+      if (event.key == 'Escape') {
+        setState(() {
+          imageIndex = 0;
         });
-      }
-      particleCooldown++;
-    }
-
-    timer ??= Timer.periodic(Duration(milliseconds: 50), (_) {
-      if (activated) return;
-      setState(() {
-        activation--;
-      });
-      if (activation <= 0) {
-        activation = 0;
-        timer?.cancel();
-        timer = null;
+        sub.cancel();
       }
     });
   }
 
-  var jasperImageIndex = 1;
+  void changeJasprText() {
+    final walker = web.document.createTreeWalker(web.document.body!, 0x4);
 
-  void addJasper() {
-    if (jasperImageIndex >= 4) return;
-    setState(() {
-      jasperImageIndex++;
-    });
+    while (walker.nextNode() != null) {
+      var node = walker.currentNode as web.Text;
+      node.textContent = node.textContent?.replaceAll('Jaspr', 'Jasper').replaceAll('jaspr', 'jasper');
+    }
   }
 
   @override
   Iterable<Component> build(BuildContext context) sync* {
-    if (activated) {
-      yield DomComponent.wrap(
-        events: {
-          'click': (event) {
-            event.preventDefault();
-            addJasper();
-          },
-        },
-        styles: Styles.box(position: Position.relative(zIndex: ZIndex(100))),
-        child: LinkButton.outlined(label: 'Meet Jasper', icon: 'jasper', to: '#meet'),
-      );
+    if (notifier.done) {
+      if (imageIndex > 0) {
+        yield Document.body(attributes: {'style': 'overflow: hidden;'});
+        yield LinkButton.outlined(label: 'Meet Jasper', icon: 'jasper', to: '#meet');
 
-      for (var i = 1; i < jasperImageIndex; i++) {
-        yield img(classes: "jasper-image", src: 'images/jasper/0$i.jpg', alt: 'Jasper');
+        yield div(classes: 'blur-backdrop', events: events(onClick: () {
+          showRandomImage();
+        }), [
+          img(
+              classes: "jasper-image",
+              src: 'images/jasper_resized/${imageIndex.toString().padLeft(2, '0')}.webp',
+              alt: 'Jasper'),
+          span([text('Click anywhere to see another image. Press ESC to close.')]),
+        ]);
+      } else {
+        yield Document.body(attributes: {'style': 'overflow: initial;'});
+        yield DomComponent.wrap(
+          events: {
+            'click': (event) {
+              event.preventDefault();
+              showRandomImage();
+            },
+          },
+          child: LinkButton.outlined(label: 'Meet Jasper', icon: 'jasper', to: '#meet'),
+        );
       }
 
       return;
@@ -117,24 +99,25 @@ class MeetJasprButtonState extends State<MeetJasprButton> {
       DomComponent.wrap(
         events: {
           'mousemove': (event) {
-            var e = event as MouseEvent;
+            var e = event as web.MouseEvent;
             var movement = (e.movementX.abs() + e.movementY.abs()) / 10;
-            onActivate(movement);
+            notifier.add(movement);
           },
           'click': (event) {
             event.preventDefault();
-            document.querySelector('#meet')?.scrollIntoView(ScrollOptions(behavior: 'smooth'));
+            web.document.querySelector('#meet')?.scrollIntoView(web.ScrollOptions(behavior: 'smooth'));
           },
         },
-        styles: progress > 0
+        styles: notifier.progressAfterCliff > 0
             ? Styles.raw({
-                'background': 'linear-gradient(to right, ${primaryMid.value}44 ${progress - 1}%, whitesmoke $progress%)'
+                'background':
+                    'linear-gradient(to right, ${primaryMid.value}44 ${notifier.progressAfterCliff - 1}%, whitesmoke ${notifier.progressAfterCliff}%)'
               })
             : null,
         child: LinkButton.outlined(label: 'Meet Jaspr', icon: 'jaspr', to: '#meet'),
       ),
       svg([
-        for (final particle in particles)
+        for (final particle in notifier.particles)
           DomComponent(
             key: ValueKey(particle.id),
             tag: 'g',
@@ -143,7 +126,6 @@ class MeetJasprButtonState extends State<MeetJasprButton> {
                 Transform.translate(x: particle.dx.percent, y: particle.dy.percent),
                 Transform.rotate(((particle.dy / 100 - 0.5) * 120).deg),
               ]),
-              // opacity: (4-particle.size) / 3,
             ).raw({'--particle-offset': '${particle.offset}px'}),
             children: [
               circle(cx: "0", cy: "0", r: "${particle.size}", fill: primaryMid, []),
@@ -168,14 +150,26 @@ class MeetJasprButtonState extends State<MeetJasprButton> {
         css('circle').raw({'animation': 'particle 1s linear forwards'}),
       ]),
     ]),
-    css('.jasper-image', [
+    css('.blur-backdrop', [
       css('&')
           .box(
-        position: Position.absolute(top: 10.percent, left: 10.percent),
-        width: 80.percent,
-        height: 80.percent,
-        
-      ).raw({'object-fit': 'contain'}),
+            position:
+                Position.fixed(top: Unit.zero, left: Unit.zero, right: Unit.zero, bottom: Unit.zero, zIndex: ZIndex(1)),
+          )
+          .background(color: Color.hex('#FFF4'))
+          .flexbox(
+              direction: FlexDirection.column, alignItems: AlignItems.center, justifyContent: JustifyContent.center)
+          .raw({'backdrop-filter': 'blur(5px)', 'user-select': 'none'}),
+      css('span').box(display: Display.inlineBlock, margin: EdgeInsets.only(top: 1.rem)).combine(bodySmall),
+      css('.jasper-image', [
+        css('&')
+            .box(
+          maxWidth: 80.percent,
+          maxHeight: 80.percent,
+          radius: BorderRadius.circular(20.px),
+        )
+            .raw({'object-fit': 'cover', 'pointer-events': 'none'}),
+      ]),
     ]),
     css.keyframes('particle', {
       '0%': Styles.box(transform: Transform.translate(y: 0.px)),
@@ -183,4 +177,67 @@ class MeetJasprButtonState extends State<MeetJasprButton> {
       '100%': Styles.box(transform: Transform.translate(x: Unit.variable('--particle-offset')), opacity: 0),
     })
   ];
+}
+
+class ProgressNotifier extends ValueNotifier<double> {
+  ProgressNotifier() : super(0);
+
+  bool get done => value >= 100;
+
+  int get progressAfterCliff => done ? 100 : max((value - 10) / 0.9, 0).round();
+
+  Timer? timer;
+
+  var particleCooldown = 0.0;
+  final List<({String id, int dx, int dy, int offset, double size})> particles = [];
+
+  final random = Random();
+
+  String randomId() {
+    return random.nextInt(0xFFFF).toRadixString(16);
+  }
+
+  void add(double v) {
+    if (done) return;
+    if (value >= 100) {
+      value = 100;
+      timer?.cancel();
+      timer = null;
+      return;
+    }
+
+    value += v * min(0.9, (1.3 - value / 100));
+
+    if (progressAfterCliff > 0) {
+      if (particleCooldown > 1) {
+        particleCooldown = 0;
+      }
+      if (particleCooldown == 0) {
+        var particle = (
+          id: randomId(),
+          dx: progressAfterCliff,
+          dy: random.nextInt(100),
+          offset: 10 + random.nextInt(40),
+          size: 0.5 + random.nextInt(3) / 2,
+        );
+        particles.add(particle);
+
+        Future.delayed(Duration(seconds: 1), () {
+          particles.remove(particle);
+          notifyListeners();
+        });
+      }
+      particleCooldown += v;
+    }
+
+    timer ??= Timer.periodic(Duration(milliseconds: 50), (_) {
+      if (done) return;
+      value--;
+      if (value <= 0) {
+        value = 0;
+        timer?.cancel();
+        timer = null;
+      }
+    });
+  }
 }
