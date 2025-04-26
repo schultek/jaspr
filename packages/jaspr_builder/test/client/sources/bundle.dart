ae3ca28ecc683a5f95c9833115a0a11771553c32
Kilian Schulte <git@schultek.dev>
2025-04-26 17:04:47 +0200

    fix linting

--- packages/jaspr_builder/test/client/sources/bundle.dart
+++ packages/jaspr_builder/test/client/sources/bundle.dart
@@ -7,10 +7,10 @@ final clientBundleOutputs = {
       "id": ["site", "lib/component_basic.dart"],
       "import": "package:site/component_basic.dart",
       "params": [
-        {"name": "a", "isNamed": false, "decoder": "p[\'a\']", "encoder": "c.a"},
-        {"name": "b", "isNamed": true, "decoder": "p[\'b\']", "encoder": "c.b"},
-        {"name": "c", "isNamed": true, "decoder": "p[\'c\']", "encoder": "c.c"},
-        {"name": "d", "isNamed": true, "decoder": "p[\'d\']", "encoder": "c.d"}
+        {"name": "a", "isNamed": false, "decoder": "p['a']", "encoder": "c.a"},
+        {"name": "b", "isNamed": true, "decoder": "p['b']", "encoder": "c.b"},
+        {"name": "c", "isNamed": true, "decoder": "p['c']", "encoder": "c.c"},
+        {"name": "d", "isNamed": true, "decoder": "p['d']", "encoder": "c.d"}
       ]
     },
     {
@@ -18,11 +18,11 @@ final clientBundleOutputs = {
       "id": ["site", "lib/component_model_class.dart"],
       "import": "package:site/component_model_class.dart",
       "params": [
-        {"name": "a", "isNamed": false, "decoder": "p[\'a\']", "encoder": "c.a"},
+        {"name": "a", "isNamed": false, "decoder": "p['a']", "encoder": "c.a"},
         {
           "name": "b",
           "isNamed": true,
-          "decoder": "[[package:site/model_class.dart]].ModelA.fromRaw(p[\'b\'])",
+          "decoder": "[[package:site/model_class.dart]].ModelA.fromRaw(p['b'])",
           "encoder": "c.b.toRaw()"
         }
       ]
@@ -32,11 +32,11 @@ final clientBundleOutputs = {
       "id": ["site", "lib/component_model_extension.dart"],
       "import": "package:site/component_model_extension.dart",
       "params": [
-        {"name": "a", "isNamed": false, "decoder": "p[\'a\']", "encoder": "c.a"},
+        {"name": "a", "isNamed": false, "decoder": "p['a']", "encoder": "c.a"},
         {
           "name": "b",
           "isNamed": true,
-          "decoder": "[[package:site/model_extension.dart]].ModelBCodec.fromRaw(p[\'b\'])",
+          "decoder": "[[package:site/model_extension.dart]].ModelBCodec.fromRaw(p['b'])",
           "encoder": "[[package:site/model_extension.dart]].ModelBCodec(c.b).toRaw()"
         }
       ]
