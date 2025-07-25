944ac1c810405ab47be250ed2dfddcc0763fa83c
Kilian Schulte <git@schultek.dev>
2025-07-25 17:17:58 +0200

    Select folder for jaspr serve command

--- modules/jaspr-code/src/extension.ts
+++ modules/jaspr-code/src/extension.ts
@@ -8,7 +8,10 @@ import {
 import { createJasprProject, handleNewProjects } from "./create";
 import { jasprClean, jasprDoctor } from "./commands";
 import { JasprDaemonProcess } from "./daemon";
-import { findJasprProjectFolders } from "./helpers/project_helper";
+import {
+  findJasprProjectFolders,
+  getFolderToRunCommandIn,
+} from "./helpers/project_helper";
 
 export async function activate(context: vscode.ExtensionContext) {
   let projects = await findJasprProjectFolders();
@@ -58,10 +61,22 @@ export async function activate(context: vscode.ExtensionContext) {
 
   context.subscriptions.push(
     vscode.commands.registerCommand("jaspr.serve", async () => {
+      const folderToRunCommandIn = await getFolderToRunCommandIn(
+        `Select the folder to run "jaspr serve" in`
+      );
+      if (!folderToRunCommandIn) {
+        return;
+      }
+
       const process = new JasprDaemonProcess();
       context.subscriptions.push(process);
 
-      process.start(context);
+      process.start(context, undefined, {
+        name: "Jaspr",
+        request: "launch",
+        type: "jaspr",
+        cwd: folderToRunCommandIn,
+      });
     })
   );
 }
