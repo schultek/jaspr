6abd0eb364fdf33dd56a5a90de520d7dfcab9b7b
ShubhamVG <lav@brainiacs.in>
2025-09-01 16:18:22 +0530

    Add and refactor tests for Filter

--- packages/jaspr/test/foundation/styles/groups/groups_test.dart
+++ packages/jaspr/test/foundation/styles/groups/groups_test.dart
@@ -149,6 +149,42 @@ void main() {
           transform: Transform.scale(2),
           shadow: BoxShadow(
               offsetX: Unit.zero, offsetY: Unit.zero, blur: Unit.pixels(10)),
+          cursor: Cursor.crosshair,
+          transition: Transition('width', duration: 500),
+        );
+
+        expect(
+          styles.properties,
+          equals({
+            'padding': '20px',
+            'margin': '0',
+            'display': 'inline-block',
+            'box-sizing': 'border-box',
+            'width': '80%',
+            'height': '70%',
+            'max-width': '1000px',
+            'border-top-style': 'solid',
+            'border-top-color': 'green',
+            'border-radius': '20px',
+            'outline-color': 'yellow',
+            'overflow': 'visible',
+            'visibility': 'visible',
+            'position': 'absolute',
+            'top': '100px',
+            'z-index': '100',
+            'opacity': '0.5',
+            'transform': 'scale(2)',
+            'box-shadow': '0 0 10px',
+            'cursor': 'crosshair',
+            'transition': 'width 500ms',
+          }),
+        );
+      });
+    });
+
+    group('filter', () {
+      test('outputs all properties', () {
+        var styles = const Styles(
           filter: Filter.list([
             Filter.blur(),
             Filter.brightness(),
@@ -179,138 +215,155 @@ void main() {
             Filter.saturate(0.9),
             Filter.url("xyz.svg#urltest"),
           ]),
-          cursor: Cursor.crosshair,
-          transition: Transition('width', duration: 500),
         );
 
         expect(
           styles.properties,
           equals({
-            'padding': '20px',
-            'margin': '0',
-            'display': 'inline-block',
-            'box-sizing': 'border-box',
-            'width': '80%',
-            'height': '70%',
-            'max-width': '1000px',
-            'border-top-style': 'solid',
-            'border-top-color': 'green',
-            'border-radius': '20px',
-            'outline-color': 'yellow',
-            'overflow': 'visible',
-            'visibility': 'visible',
-            'position': 'absolute',
-            'top': '100px',
-            'z-index': '100',
-            'opacity': '0.5',
-            'transform': 'scale(2)',
-            'box-shadow': '0 0 10px',
             'filter': 'blur(0) brightness(1) contrast(1) '
                 'drop-shadow(1rem 1rem) grayscale(1) hue-rotate(0) '
                 'invert(1) opacity(1) sepia(1) saturate(1) url(abc.svg#urltest)',
             'backdrop-filter': 'blur(0.1rem) brightness(0.2) contrast(0.3) '
                 'drop-shadow(1rem 1rem 0.4rem red) grayscale(0.5) hue-rotate(45deg) '
                 'invert(0.6) opacity(0.7) sepia(0.8) saturate(0.9) url(xyz.svg#urltest)',
-            'cursor': 'crosshair',
-            'transition': 'width 500ms',
           }),
         );
+      });
+
+      test("Filter.list does not allow empty list", () {
+        final filter = Filter.list([]);
+        expect(
+          () => filter.value,
+          throwsA(predicate((e) {
+            return e == 'Filter.list cannot be empty.';
+          })),
+        );
+      });
+
+      test("Filter.list does not allow nesting another Filter.list", () {
+        final nestedFilter = Filter.list([Filter.list([])]);
+        expect(
+          () => nestedFilter.value,
+          throwsA(predicate((e) {
+            return e == 'Cannot nest [Filter.list] inside [Filter.list].';
+          })),
+        );
+      });
+
+      test("Filter.list does not allow non-listable members", () {
+        for (final val in const <Filter>[
+          Filter.none,
+          Filter.initial,
+          Filter.revert,
+          Filter.revertLayer,
+          Filter.unset,
+        ]) {
+          final filter = Filter.list([val]);
+          expect(
+            () => filter.value,
+            throwsA(predicate((e) {
+              return e ==
+                  'Cannot use ${val.value} as a filter list item, '
+                      'only standalone use supported.';
+            })),
+          );
+        }
       });
     });
+  });
 
-    group('flexbox', () {
-      test('outputs all properties', () {
-        var styles = const Styles(
-          flexDirection: FlexDirection.column,
-          flexWrap: FlexWrap.nowrap,
-          justifyContent: JustifyContent.center,
-          alignItems: AlignItems.start,
-          alignContent: AlignContent.center,
-        );
+  group('flexbox', () {
+    test('outputs all properties', () {
+      var styles = const Styles(
+        flexDirection: FlexDirection.column,
+        flexWrap: FlexWrap.nowrap,
+        justifyContent: JustifyContent.center,
+        alignItems: AlignItems.start,
+        alignContent: AlignContent.center,
+      );
 
-        expect(
-          styles.properties,
-          equals({
-            'flex-direction': 'column',
-            'flex-wrap': 'nowrap',
-            'justify-content': 'center',
-            'align-items': 'start',
-            'align-content': 'center',
-          }),
-        );
-      });
+      expect(
+        styles.properties,
+        equals({
+          'flex-direction': 'column',
+          'flex-wrap': 'nowrap',
+          'justify-content': 'center',
+          'align-items': 'start',
+          'align-content': 'center',
+        }),
+      );
     });
+  });
 
-    group('flexitem', () {
-      test('outputs all properties', () {
-        var styles = const Styles(
-          flex: Flex(grow: 2, shrink: 1, basis: Unit.auto),
-          order: 2,
-          alignSelf: AlignSelf.start,
-        );
+  group('flexitem', () {
+    test('outputs all properties', () {
+      var styles = const Styles(
+        flex: Flex(grow: 2, shrink: 1, basis: Unit.auto),
+        order: 2,
+        alignSelf: AlignSelf.start,
+      );
 
-        expect(
-          styles.properties,
-          equals({
-            'flex-grow': '2',
-            'flex-shrink': '1',
-            'flex-basis': 'auto',
-            'order': '2',
-            'align-self': 'start',
-          }),
-        );
-      });
+      expect(
+        styles.properties,
+        equals({
+          'flex-grow': '2',
+          'flex-shrink': '1',
+          'flex-basis': 'auto',
+          'order': '2',
+          'align-self': 'start',
+        }),
+      );
     });
+  });
 
-    group('grid', () {
-      test('outputs all properties', () {
-        var styles = const Styles(
-          gridTemplate: GridTemplate(
-              areas: GridAreas([
-            'header header',
-            'side content',
-            'side content',
-          ])),
-          gap: Gap(row: Unit.pixels(20)),
-          autoRows: [TrackSize(Unit.percent(20)), TrackSize.auto],
-          autoColumns: [
-            TrackSize(Unit.pixels(100)),
-            TrackSize.auto,
-            TrackSize.auto
-          ],
-          justifyItems: JustifyItems.center,
-        );
+  group('grid', () {
+    test('outputs all properties', () {
+      var styles = const Styles(
+        gridTemplate: GridTemplate(
+            areas: GridAreas([
+          'header header',
+          'side content',
+          'side content',
+        ])),
+        gap: Gap(row: Unit.pixels(20)),
+        autoRows: [TrackSize(Unit.percent(20)), TrackSize.auto],
+        autoColumns: [
+          TrackSize(Unit.pixels(100)),
+          TrackSize.auto,
+          TrackSize.auto
+        ],
+        justifyItems: JustifyItems.center,
+      );
 
-        expect(
-          styles.properties,
-          equals({
-            'grid-template-areas': '"header header"\n'
-                '"side content"\n'
-                '"side content"',
-            'grid-auto-rows': '20% auto',
-            'grid-auto-columns': '100px auto auto',
-            'row-gap': '20px',
-            'justify-items': 'center',
-          }),
-        );
-      });
+      expect(
+        styles.properties,
+        equals({
+          'grid-template-areas': '"header header"\n'
+              '"side content"\n'
+              '"side content"',
+          'grid-auto-rows': '20% auto',
+          'grid-auto-columns': '100px auto auto',
+          'row-gap': '20px',
+          'justify-items': 'center',
+        }),
+      );
     });
+  });
 
-    group('griditem', () {
-      test('outputs all properties', () {
-        var styles = const Styles(
-          gridPlacement: GridPlacement.area('content'),
-          justifySelf: JustifySelf.start,
-        );
+  group('griditem', () {
+    test('outputs all properties', () {
+      var styles = const Styles(
+        gridPlacement: GridPlacement.area('content'),
+        justifySelf: JustifySelf.start,
+      );
 
-        expect(
-          styles.properties,
-          equals({
-            'grid-area': 'content',
-            'justify-self': 'start',
-          }),
-        );
-      });
+      expect(
+        styles.properties,
+        equals({
+          'grid-area': 'content',
+          'justify-self': 'start',
+        }),
+      );
     });
   });
 }
