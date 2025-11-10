f416b620800a7462e6eccfca556c3ea1104f71a1
Parker Lougheed <parlough@gmail.com>
2025-11-10 13:16:33 -0600

    [jaspr] Deprecate the package:jaspr/ui.dart library

--- packages/jaspr/lib/ui.dart
+++ packages/jaspr/lib/ui.dart
@@ -1,3 +1,8 @@
+@Deprecated(
+  "This library is deprecated and will be removed in a future release. "
+  "Import 'package:jaspr/jaspr.dart' directly instead. "
+  "For the utility components, build your own or use a component library.",
+)
 library;
 
 export 'jaspr.dart' hide runApp, Padding;
