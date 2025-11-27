ef6363587719d910479e614828106262c6ff79d5
Parker Lougheed <parlough@gmail.com>
2025-11-27 04:07:46 -0600

    Avoid unnecessarily exposing `RawText` support types (#670)

--- packages/jaspr/lib/src/dom/raw_text/raw_text_vm.dart
+++ packages/jaspr/lib/src/dom/raw_text/raw_text_vm.dart
@@ -1,21 +1,27 @@
 import '/server.dart';
 
-/// Renders its input as raw HTML.
+/// Renders its [text] input as raw HTML.
 ///
+/// {@template jaspr.rawText.warning}
 /// **WARNING**: This component does not escape any
 /// user input and is vulnerable to [cross-site scripting (XSS) attacks](https://owasp.org/www-community/attacks/xss/).
 /// Make sure to sanitize any user input when using this component.
-class RawText extends Component {
+/// {@endtemplate}
+final class RawText extends Component {
+  /// Creates a component that renders [text] as raw HTML.
+  ///
+  /// {@macro jaspr.rawText.warning}
   const RawText(this.text, {super.key});
 
+  /// The text to render as raw HTML.
   final String text;
 
   @override
-  Element createElement() => RawTextElement(this);
+  Element createElement() => _RawTextElement(this);
 }
 
-class RawTextElement extends LeafRenderObjectElement {
-  RawTextElement(super.component);
+class _RawTextElement extends LeafRenderObjectElement {
+  _RawTextElement(super.component);
 
   @override
   RenderObject createRenderObject() {
