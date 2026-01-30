8170f61d0599df163f847de4c3ecc6b720a1ed5d
Mahdi <mkfard@outlook.com>
2026-01-30 12:55:40 +0330

    Added docs comments.

--- packages/jaspr_cli/lib/src/utils.dart
+++ packages/jaspr_cli/lib/src/utils.dart
@@ -2,6 +2,9 @@ import 'dart:io';
 
 const _PUB_HOSTED_URL_ENV_NAME = 'PUB_HOSTED_URL';
 
+/// Gets the value of `PUB_HOSTED_URL` enviroment variable. Returns `null` if not found.
+///
+/// Returning `null` will fallback to https://pub.dev.
 String? getPubDevUrl() {
   for (final kv in Platform.environment.entries) {
     if (kv.key.toUpperCase() == _PUB_HOSTED_URL_ENV_NAME) {
