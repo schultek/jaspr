4ed044a4de120607fdd58e4d969164dd6c2ca657
Mahdi <mkfard@outlook.com>
2026-02-04 14:13:50 +0330

    Fixed a linter error.

--- packages/jaspr_cli/lib/src/utils.dart
+++ packages/jaspr_cli/lib/src/utils.dart
@@ -1,16 +1,9 @@
 import 'dart:io';
 
-const _PUB_HOSTED_URL_ENV_NAME = 'PUB_HOSTED_URL';
-
 /// Gets the base url for pub mirror with respect to the value of `PUB_HOSTED_URL` enviroment variable.
 ///
 /// Falls back to https://pub.dev if the enviroment variable is not found.
 String getPubDevBaseUrl() {
-  for (final kv in Platform.environment.entries) {
-    if (kv.key.toUpperCase() == _PUB_HOSTED_URL_ENV_NAME) {
-      return kv.value;
-    }
-  }
-
-  return 'https://pub.dev';
+  final mirror = Platform.environment['PUB_HOSTED_URL'];
+  return mirror ?? 'https://pub.dev';
 }
