8a0092cd922b77038aa6ae1973047daa9ec6dd65
Kilian Schulte <git@schultek.dev>
2026-03-12 22:09:16 +0100

    add one-off convert-html command to tooling daemon

--- packages/jaspr_cli/lib/src/commands/tooling_daemon_command.dart
+++ packages/jaspr_cli/lib/src/commands/tooling_daemon_command.dart
@@ -3,6 +3,9 @@ import 'dart:convert';
 import 'dart:io';
 import 'dart:math';
 
+import 'package:args/args.dart';
+import 'package:async/async.dart';
+import 'package:http/http.dart' as http;
 import 'package:path/path.dart' as path;
 import 'package:web_socket_channel/web_socket_channel.dart';
 
@@ -15,7 +18,14 @@ import 'base_command.dart';
 
 class ToolingDaemonCommand extends BaseCommand with DaemonHelper {
   ToolingDaemonCommand() : super(logger: DaemonLogger()) {
-    argParser.addFlag('start', hide: true);
+    argParser.addCommand('start');
+    argParser.addCommand(
+      'convert-html',
+      ArgParser()
+        ..addOption('file')
+        ..addOption('url')
+        ..addOption('query'),
+    );
   }
 
   @override
@@ -35,7 +45,37 @@ class ToolingDaemonCommand extends BaseCommand with DaemonHelper {
 
   @override
   Future<int> runCommand() async {
-    if (argResults!.flag('start')) {
+    final command = argResults?.command?.name;
+
+    if (command == 'convert-html') {
+      final file = argResults?.command?.option('file');
+      final url = argResults?.command?.option('url');
+      final query = argResults?.command?.option('query');
+
+      Map<String, Object?> params;
+      if (file != null && url == null) {
+        final html = File(file).readAsStringSync();
+        params = {
+          'html': html,
+          'query': ?query,
+        };
+      } else if (file == null && url != null) {
+        final response = await http.get(Uri.parse(url));
+        params = {
+          'html': response.body,
+          'query': ?query,
+        };
+      } else {
+        stderr.writeln('Either --file or --url must be provided.');
+        return 1;
+      }
+
+      return runToolingDaemonCommand('html.convert', params, (response) {
+        return response.toString();
+      });
+    }
+
+    if (command == 'start') {
       return runWithWebSocketDaemon(
         (daemon) async {
           daemon.registerDomain(ScopesDomain(daemon, logger));
@@ -55,33 +95,7 @@ class ToolingDaemonCommand extends BaseCommand with DaemonHelper {
       );
     }
 
-    var runningPort = await findRunningDaemon();
-    if (runningPort != null) {
-      stdout.writeln(
-        jsonEncode([
-          {'status': 'running', 'port': runningPort},
-        ]),
-      );
-      return 0;
-    }
-
-    final args = Platform.resolvedExecutable.endsWith('/dart')
-        ? [...Platform.executableArguments, Platform.script.path, 'tooling-daemon', '--start']
-        : ['tooling-daemon', '--start'];
-
-    await Process.start(
-      Platform.resolvedExecutable,
-      args,
-      mode: ProcessStartMode.detached,
-    );
-
-    final sw = Stopwatch()..start();
-    while (sw.elapsed < const Duration(seconds: 2)) {
-      runningPort = await findRunningDaemon();
-      if (runningPort != null) break;
-      await Future<void>.delayed(const Duration(milliseconds: 100));
-    }
-
+    final runningPort = await findOrStartDaemon();
     if (runningPort == null) {
       stdout.writeln(
         jsonEncode([
@@ -99,6 +113,32 @@ class ToolingDaemonCommand extends BaseCommand with DaemonHelper {
     }
   }
 
+  Future<int?> findOrStartDaemon() async {
+    var runningPort = await findRunningDaemon();
+    if (runningPort != null) {
+      return runningPort;
+    }
+
+    final args = Platform.resolvedExecutable.endsWith('/dart')
+        ? [...Platform.executableArguments, Platform.script.path, 'tooling-daemon', 'start']
+        : ['tooling-daemon', 'start'];
+
+    await Process.start(
+      Platform.resolvedExecutable,
+      args,
+      mode: ProcessStartMode.detached,
+    );
+
+    final sw = Stopwatch()..start();
+    while (sw.elapsed < const Duration(seconds: 2)) {
+      runningPort = await findRunningDaemon();
+      if (runningPort != null) break;
+      await Future<void>.delayed(const Duration(milliseconds: 100));
+    }
+
+    return runningPort;
+  }
+
   Future<int?> findRunningDaemon() async {
     final lockFile = _getLockFile();
     if (!lockFile.existsSync()) return null;
@@ -106,31 +146,8 @@ class ToolingDaemonCommand extends BaseCommand with DaemonHelper {
       final data = jsonDecode(lockFile.readAsStringSync());
       if (data case {'pid': final int pid, 'port': final int port}) {
         try {
-          final channel = WebSocketChannel.connect(Uri.parse('ws://localhost:$port'));
-          final completer = Completer<bool>();
-          final id = Random().nextInt(10000);
-
-          channel.sink.add(
-            jsonEncode([
-              {'id': id, 'method': 'daemon.version'},
-            ]),
-          );
-
-          final sub = channel.stream
-              .map((msg) => msg is String ? jsonDecode(msg) : null)
-              .where((data) {
-                if (data case [{'id': final int msgId}] when msgId == id) return true;
-                return false;
-              })
-              .listen((data) => completer.complete(true), onError: (e) => completer.complete(false));
-
-          final success = await completer.future.timeout(const Duration(seconds: 2), onTimeout: () => false);
-          await sub.cancel();
-          await channel.sink.close();
-
-          if (success == true) {
-            return port;
-          }
+          final response = await sendSingleCommandToDaemon(port, 'daemon.version', {});
+          if (response != null) return port;
         } catch (_) {}
 
         // If we are here, either the process is dead or the WebSocket is not responding.
@@ -151,6 +168,61 @@ class ToolingDaemonCommand extends BaseCommand with DaemonHelper {
   File _getLockFile() {
     final settingsDir = getSettingsDirectory();
     return File(path.join(settingsDir.path, 'tooling-daemon.json'));
+  }
+
+  Future<int> runToolingDaemonCommand(
+    String method,
+    Map<String, Object?> params,
+    String Function(Object) resultBuilder,
+  ) async {
+    final runningPort = await findOrStartDaemon();
+    if (runningPort == null) {
+      stderr.writeln('Failed to start tooling daemon. Please try again.');
+      return 1;
+    }
+
+    try {
+      final response = await sendSingleCommandToDaemon(runningPort, method, params);
+      if (response != null) {
+        stdout.writeln(resultBuilder(response));
+        return 0;
+      } else {
+        stderr.writeln('Failed to get result from tooling daemon.');
+        return 1;
+      }
+    } catch (e) {
+      stderr.writeln('Failed to get result from tooling daemon: $e');
+      return 1;
+    }
+  }
+
+  Future<Object?> sendSingleCommandToDaemon(
+    int daemonPort,
+    String method,
+    Map<String, Object?> params, {
+    Duration timeout = const Duration(seconds: 2),
+  }) async {
+    final channel = WebSocketChannel.connect(Uri.parse('ws://localhost:$daemonPort'));
+    final id = Random().nextInt(10000);
+
+    channel.sink.add(
+      jsonEncode([
+        {'id': id, 'method': method, 'params': params},
+      ]),
+    );
+
+    final response = await channel.stream
+        .map((msg) => msg is String ? jsonDecode(msg) : null)
+        .map((data) {
+          if (data case [{'id': final int msgId, 'result': final result}] when msgId == id) return result;
+          return null;
+        })
+        .where((data) => data != null)
+        .firstOrNull
+        .timeout(timeout);
+
+    await channel.sink.close();
+    return response;
   }
 }
 
